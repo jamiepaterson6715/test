760bfcacd26f83dd1556891814bed8a71aa24fbe
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-05-10 11:48:28 +0100

    codecademy_experiment

---
+++ exp1.rb
@@ -0,0 +1,33 @@
+# print "Thtring, pleathe!: "
+# user_input = gets.chomp
+# user_input.downcase!
+#
+# if user_input.include? "s"
+#   user_input.gsub!(/s/, "th")
+# else
+#   puts "Nothing to do here!"
+# end
+#
+# puts "Your string is: #{user_input}"
+
+# i = 1
+# while i < 11
+#   puts i
+#   i = i + 1
+# end
+
+# i = 0
+# until i == 6
+#   puts i
+#   i += 1
+# end
+
+# for i in 1...10 # ... tells ruby to exclude the last number (here 10 if we .. only then it includes the last num)
+#   puts i
+# end
+
+counter = 1
+while counter < 11
+  puts counter
+ counter = counter + 1
+end
---
+++ exp1_spec.rb
@@ -0,0 +1,5 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( cd. '../exp1.rb' )
+
+class FunctionsTest < MiniTest::Test
